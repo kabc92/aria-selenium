54109fb0cb7af383fcbe59be585de86a88db7297
Kevin Barragan <kabc91@gmail.com>
2026-07-06 22:04:34 -0500

    Add Page Object Model: LoginPage with locators and login action

---
+++ src/test/java/com/aria/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package com.aria.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    //Instance variable only accessible from this class
+    private WebDriver driver;
+
+    //CONSTRUCTOR - recibe el driver de BaseTest
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    //LOCATORS - Los elementos de la pagina
+    private By usernameField = By.id("user-name");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("login-button");
+
+    //Actions - Lo que puedes hacer en esta pagina
+    public void login(String username, String password)
+    {
+        driver.findElement(usernameField).sendKeys(username);
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(loginButton).click();
+    }
+
+    public String getTitle(){
+        return driver.getTitle();
+    }
+
+}
--- src/test/java/com/aria/tests/LoginTest.java
+++ src/test/java/com/aria/tests/LoginTest.java
@@ -1,14 +1,23 @@
 package com.aria.tests;
 
 import com.aria.base.BaseTest;
+import com.aria.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
+
+
+
     @Test
     public void openURL(){
-        driver.navigate().to("https://www.saucedemo.com");
-        driver.getTitle();
 
+        driver.get("https://www.saucedemo.com");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+
+        System.out.println(loginPage.getTitle());
     }
 }
